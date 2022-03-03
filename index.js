3b47543dd447e12c598c142bea4acc913d591280
Marcin Myśliwiec <marcin.mysliw@gmail.com>
2022-03-03 13:31:28 +0100

    working

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const {Server} = require("socket.io");
+app.use(cors());
+
+const PORT = 3001;
+const server = http.createServer(app);
+let onlineUsers = [];
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const getUsersFromRoom = (room) => {
+  return onlineUsers.filter(obj => obj.room === room);
+}
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", ({username, room}) => {
+    socket.join(room);
+
+    const userData = {socketId: socket.id, username, room}
+    onlineUsers.push(userData);
+    // console.log(`User ${username} - ${socket.id} connected to ${room}`);
+
+    socket.to(room).emit("receive_message", {
+      room: room,
+      author: {name: 'BOT', is_bot: true},
+      type: 'CONNECTION',
+      message: `${username} has joined`,
+      time: new Date().toISOString()
+    });
+    socket.to(room).emit("users_in_room", onlineUsers);
+  });
+
+  socket.on("send_message", (msgObj) => {
+    socket.to(msgObj.room).emit("receive_message", msgObj);
+  });
+
+  socket.on("disconnect", () => {
+    const index = onlineUsers.findIndex(obj => obj.socketId === socket.id);
+    const disconnectedUser = onlineUsers[index];
+    console.log('disconnect ', disconnectedUser)
+    if (index < 0) return 0;
+
+    onlineUsers = onlineUsers.filter(obj => obj.socketId !== socket.id);
+    socket.to(disconnectedUser.room).emit("receive_message", {
+      room: disconnectedUser.room,
+      author: {name: 'BOT', is_bot: true},
+      type: 'DISCONNECTION',
+      message: `${disconnectedUser.username} has left`,
+      time: new Date().toISOString()
+    });
+    socket.to(disconnectedUser.room).emit("users_in_room", getUsersFromRoom(disconnectedUser.room));
+  });
+});
+
+app.get('/users', (req, res) => {
+  return res.status(200).json(getUsersFromRoom(req.query.room));
+});
+
+server.listen(PORT, () => {
+  console.log("SERVER RUNNING");
+});
