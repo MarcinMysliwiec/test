8835f01d6b27eeff0a153e58c6a579cd8f1a63c7
Marcin Myśliwiec <marcin.mysliw@gmail.com>
2022-06-06 20:37:13 +0200

    asd

--- index.js
+++ index.js
@@ -49,9 +49,9 @@ io.on("connection", (socket) => {
   });
 
   socket.on("send_message", (msg) => {
-    const msgObj = JSON.parse(msg);
-    console.log("send_message", msgObj);
-    socket.to(GLOBAL_ROOM).emit("receive_message", msgObj);
+    // const msgObj = JSON.parse(msg);
+    console.log("send_message", msg);
+    socket.to(GLOBAL_ROOM).emit("receive_message", msg);
   });
 
   socket.on("disconnect", () => {
